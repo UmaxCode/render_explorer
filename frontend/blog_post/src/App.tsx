95f01bbceb0e75468fdf650f14248e120867eb0f
umaxcode <maxwellodoom1729@gmail.com>
2024-08-16 14:31:40 +0000

    setup react app with tailwind css

---
+++ frontend/blog_post/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400 text-white text-xl text-center py-3 rounded-sm">
+        App is live
+      </h1>
+    </>
+  );
+}
+
+export default App;
